8929ccbd1a2a5b3c312cdf525e23c9f129e2c16d
Shatrov-Mihail <78844887@mail.ru>
2024-08-21 22:54:26 +0500

    добавки

--- my-app/src/return/list-case-button.js
+++ my-app/src/return/list-case-button.js
@@ -19,17 +19,3 @@ export const ListCaseButton = ({sortedCase, deleteCase, updateCase, setNewCase})
 		</ul>
 	  );
 	};
-// 	return(
-// 		<ul className={styles.list}>
-// 	{sortedCase.map(({ id, name }) => (
-// 		<li key={id} className={styles.listItem}>
-// 		{name}
-// 		<div className={styles.buttonList}>
-// 		  <button onClick={() => deleteCase(id)}>🗑</button>
-// 		  <button onClick={() => updateCase(id)}>✐</button>
-// 		</div>
-// 	  </li>
-// 	))}
-//   </ul>
-// )
-// }
